54c617715aef6031d2840a647cf429cddf79b6d6
KarinaPrim <karinacristinaprim077@gmail.com>
2026-03-25 15:54:55 -0300

    atuaizar

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php include 'includes/header.php'; ?>
 
+<link rel="stylesheet" href="css/style.css">
+
 <section class="home">
+
+    <!-- TEXTO ESQUERDA -->
+    <div class="home-conteudo">
+        <h1>Explore o Corpo Humano!</h1>
+        <p>
+            Descubra os principais órgãos do corpo de forma detalhada e interativa.
+        </p>
+        <button class="btn-home">Comece Agora</button>
+    </div>
 
     <!-- CORPO HUMANO -->
     <div class="home-imagem">
@@ -8,5 +19,7 @@
     </div>
 
 </section>
+
+
 
 <?php include 'includes/footer.php'; ?>
